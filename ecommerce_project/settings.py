483689d22c244a2fea9cf167bd0ad3a18f8745a6
Nency Savaliya <112814520+nencysavaliya@users.noreply.github.com>
2025-12-24 16:48:48 +0530

    Update settings.py

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'e-commerce-production-faad.up.railway.app', '.railway.app']
 
 # Application definition
 INSTALLED_APPS = [
